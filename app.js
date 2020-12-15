51eeb4b82716d9dee0bf4cfaf4ba18392c911eba
AhamedBilal <bilalazmy0@gmail.com>
2020-12-15 21:59:27 +0530

    test post req

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const formidable = require('formidable');
 const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
 
+const axios = require('axios');
 
 ffmpeg.setFfmpegPath("C:/ffmpeg/bin/ffmpeg.exe");
 ffmpeg.setFfprobePath("C:/ffmpeg/bin/ffprobe.exe");
@@ -31,90 +32,122 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.post('/convert', (req, res, next) => {
-  const form = formidable({multiples: true});
-  form.maxFileSize = 2000 * 1024 * 1024;
-  form.on('progress', (bytesReceived, bytesExpected) => {
-    console.log(bytesReceived * 100 / bytesExpected)
-  });
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      console.log(err);
-      next(err);
-      res.json('error');
-      return;
-    }
-    const oldPath = files.file.path;
-    const newPath = path.join(__dirname, 'tmp') + '\\' + files.file.name;
-    const rawData = fs.readFileSync(oldPath)
-
-    fs.writeFile(newPath, rawData, function (err) {
-      if (err) console.log(err)
-      ffmpeg(__dirname+'\\tmp\\'+files.file.name)
-          .output(path.join(__dirname, 'out') + '\\' + files.file.name.replace(/\.(mkv|avi|mp4|flv|mov)$/g, '') + '_780p.mp4')
-          .videoCodec('libx265')
-          .audioCodec('libmp3lame')
-          .audioBitrate(128)
-          .size('?x780')
-          .format('mp4')
-
-          .output(path.join(__dirname, 'out') + '\\' + files.file.name.replace(/\.(mkv|avi|mp4|flv|mov)$/g, '') + '_480p.mp4')
-          .videoCodec('libx265')
-          .audioCodec('libmp3lame')
-          .audioBitrate(128)
-          .size('?x480')
-          .format('mp4')
-
-          .output(path.join(__dirname, 'out') + '\\' + files.file.name.replace(/\.(mkv|avi|mp4|flv|mov)$/g, '') + '_360p.mp4')
-          .videoCodec('libx265')
-          .audioCodec('libmp3lame')
-          .audioBitrate(64)
-          .size('?x360')
-          .format('mp4')
-
-          .on('start', function (commandLine) {
-            console.log('Spawned Ffmpeg with command: ' + commandLine);
-          })
-          .on('codecData', function (data) {
-            console.log('Input is ' + data.audio_details + ' audio ' +
-                'with ' + data.video_details + ' video');
-          })
-          .on('progress', function (progress) {
-            console.log(progress.percent);
-          })
-          .on('error', function (error) {
-            return res.send(error);
-          })
-          .on('end', function (stdout, stderr) {
-            console.log('Transcoding succeeded !');
-            fs.unlinkSync(path.join(__dirname, 'tmp') + '/' + files.file.name);
-            res.send("Successfully uploaded");
-          })
-          .run();
+    const form = formidable({multiples: true});
+    form.maxFileSize = 2000 * 1024 * 1024;
+    form.on('progress', (bytesReceived, bytesExpected) => {
+        console.log(bytesReceived * 100 / bytesExpected)
     });
-  })
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            console.log(err);
+            next(err);
+            res.json('error');
+            return;
+        }
+        const oldPath = files.file.path;
+        const newPath = path.join(__dirname, 'tmp') + '\\' + files.file.name;
+        const rawData = fs.readFileSync(oldPath)
+
+        fs.writeFile(newPath, rawData, function (err) {
+            if (err) console.log(err)
+            ffmpeg(__dirname + '\\tmp\\' + files.file.name)
+                .output(path.join(__dirname, 'out') + '\\' + files.file.name.replace(/\.(mkv|avi|mp4|flv|mov)$/g, '') + '_780p.mp4')
+                .videoCodec('libx265')
+                .audioCodec('libmp3lame')
+                .audioBitrate(128)
+                .size('?x780')
+                .format('mp4')
+
+                .output(path.join(__dirname, 'out') + '\\' + files.file.name.replace(/\.(mkv|avi|mp4|flv|mov)$/g, '') + '_480p.mp4')
+                .videoCodec('libx265')
+                .audioCodec('libmp3lame')
+                .audioBitrate(128)
+                .size('?x480')
+                .format('mp4')
+
+                .output(path.join(__dirname, 'out') + '\\' + files.file.name.replace(/\.(mkv|avi|mp4|flv|mov)$/g, '') + '_360p.mp4')
+                .videoCodec('libx265')
+                .audioCodec('libmp3lame')
+                .audioBitrate(64)
+                .size('?x360')
+                .format('mp4')
+
+                .on('start', function (commandLine) {
+                    console.log('Spawned Ffmpeg with command: ' + commandLine);
+                })
+                .on('codecData', function (data) {
+                    console.log('Input is ' + data.audio_details + ' audio ' +
+                        'with ' + data.video_details + ' video');
+                })
+                .on('progress', function (progress) {
+                    console.log(progress.percent);
+                })
+                .on('error', function (error) {
+                    return res.send(error);
+                })
+                .on('end', function (stdout, stderr) {
+                    console.log('Transcoding succeeded !');
+                    fs.unlinkSync(path.join(__dirname, 'tmp') + '/' + files.file.name);
+                    res.send("Successfully uploaded");
+                })
+                .run();
+        });
+    })
 
 })
 
 app.get('/smil', (req, res) => {
 
+    const data = {
+        name: 'post_smil',
+        serverName: '_defaultServer_',
+        smilStreams: [
+            {
+                systemLanguage: 'eng',
+                src: 'sample.mp4',
+                audioBitrate: 44100,
+                videoBitrate: 350000,
+                width: 640,
+                height: 320
+            },
+            {
+                systemLanguage: 'eng',
+                src: 'sample2.mp4',
+                audioBitrate: 44100,
+                videoBitrate: 350000,
+                width: 640,
+                height: 320
+            }
+        ],
+        title: 'test smil post'
+    };
+
+    axios.post('http://localhost:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/vod/smilfiles', data)
+        .then(function (response) {
+            console.log(response);
+            res.json(response);
+        })
+        .catch(function (error) {
+            res.json(error);
+        });
 });
 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
